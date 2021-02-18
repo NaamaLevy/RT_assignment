ff70a0ae4196becbc540dba0fc5c3c2057dad6dd
Naama-Le <54737585+Naama-Le@users.noreply.github.com>
2021-02-18 17:57:57 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,146 @@
+
+#include <stdint.h>
+#include <stdlib.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+#include <iostream>
+#include <thread>
+#include <sys/types.h>
+#include <dirent.h>
+#include <mutex>
+#include <vector>
+
+using namespace std;
+enum allocation_type{
+    NO_ALLOCATION, SELF_ALLOCATED, STB_ALLOCATED
+};
+
+typedef  struct {
+    int width;
+    int height;
+    int channels;
+    size_t size;
+    uint8_t *data;
+    enum allocation_type allocation_;
+}Image;
+
+
+
+void Image_load(Image *img,const char *fname);
+void Image_create(Image *img, int width, int height, int channels,bool zeroed);
+void Image_save(const Image *img, const char *fname);
+void Image_free(Image *img);
+void do_something(const Image *orig, Image *conv);
+
+
+
+void Image_load(Image *img, const char *fname) {
+
+    if((img->data= stbi_load(fname, &img->width, &img->height, &img->channels, 0)) != NULL){
+        img->size = img->width * img->height * img->channels;
+        img->allocation_=STB_ALLOCATED;
+    }
+}
+
+void Image_create(Image *img, int width, int height, int channels,bool zeroed){
+    size_t size = width * height * channels;
+    if(zeroed){
+        img->data = static_cast<uint8_t *>(calloc(size, 1));
+    }
+    else{
+        img->data = static_cast<uint8_t *>(malloc(size));
+    }
+    if(img->data!=NULL){
+        img->width = width;
+        img->height = height;
+        img->size = size;
+        img->channels = channels;
+        img->allocation_ = SELF_ALLOCATED;
+    }
+}
+void Image_save(const Image *img, const char *fname){
+    stbi_write_png(fname, img->width,img->height,img->channels, img->data, img->width*img->channels);
+}
+
+void Image_free(Image *img) {
+    if(img->allocation_ != NO_ALLOCATION && img->data != NULL){
+        if(img->allocation_ == STB_ALLOCATED) { stbi_image_free(img->data);
+        } else {
+            free(img->data);
+        }
+        img->data = NULL;
+        img->width = 0;
+        img->height = 0;
+        img->size = 0;
+        img->allocation_ = NO_ALLOCATION;
+    }
+}
+
+void do_something(const Image *orig, Image *conv){
+    Image_create(conv, orig->width, orig->height, 1, false);
+    for(unsigned char *p = orig->data, *pg = conv->data; p != orig->data+orig->size; p += orig->channels, pg += conv->channels){
+        *pg = (uint8_t)((*p)*0.299 + *(p+1)*0.587+ *(p+2)*0.144);
+    }
+}
+
+
+class Task{
+private:
+    int _index;
+    char * _img;
+public:
+    Task( char *img, int index) : _img(img), _index(index){}
+
+    void run(){
+        char path[] = "images\\";
+        strcat(path, _img);
+        Image old_image;
+        Image_load(&old_image, path);
+        Image conv_image;
+        //convert the img
+        do_something(&old_image, &conv_image);
+        //save the converted img as new png file
+        char filename[12];
+        char idx[2];
+        strcpy(filename,"image_");
+        snprintf(idx,2, "%d", _index);
+        strcat(filename, idx);
+        strcat(filename,".png");
+        Image_save(&conv_image, filename);
+        //free space
+        Image_free(&old_image);
+        Image_free(&conv_image);
+    }
+
+
+};
+
+int main() {
+    //create task for each img in tasks_vector
+    DIR *dir = opendir("C:\\Users\\Naama Levy\\Desktop\\personal\\jobs\\greeneye\\cpp_project\\cmake-build-debug\\images");
+
+    int index = 1;
+    struct dirent *ent;
+    while ((ent = readdir(dir)) != NULL) {
+        if (ent->d_type == DT_REG) {
+            cout << ent->d_name << "\n";
+            char path[] = "images\\";
+            char *name = ent->d_name;
+            strcat(path, name);
+            Task *task = new Task(ent->d_name, index);
+            thread th(&Task::run, task);
+            index++;
+            th.join();
+        }
+    }
+
+
+    return 0;
+}
+
+
+
+
+
